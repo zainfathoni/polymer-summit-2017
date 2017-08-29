afb230147fe8675e17ebe819874338b1799cb85d
Zain Fathoni <zain.fathoni@gmail.com>
2017-08-30 01:59:41 +0700

    get image size

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ app.get("/", async (req, res) => {
   if (filepath.endsWith("/")) filepath += "index.html";
   const buffer = await readFile("./static/" + filepath);
   const content = buffer.toString();
-  const newContent = content.split(/(<sc-img[^>]+><\/sc-img>)/).map(item => {
-    if (!item.startsWith("<sc-img")) return item;
-    const src = /src="([^"]+)"/.exec(item)[1];
-    const img = im("./static/" + src);
-    console.log(img);
-    return item;
-  });
+  const newContent = await Promise.all(
+    content.split(/(<sc-img[^>]+><\/sc-img>)/).map(async item => {
+      if (!item.startsWith("<sc-img")) return item;
+      const src = /src="([^"]+)"/.exec(item)[1];
+      const img = im("./static/" + src);
+      const sizeFunc = promisify(img.size.bind(img));
+      const { width, height } = await sizeFunc();
+      console.log(width, height);
+      return item;
+    })
+  );
   res.send(newContent.join(""));
 });
 app.use(Express.static("static"));
