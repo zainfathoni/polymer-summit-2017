d2e11a1b2a0fa0632057cb2051cffd04ebdaa778
Zain Fathoni <zain.fathoni@gmail.com>
2017-08-30 01:56:22 +0700

    load img manually

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Express = require("express");
 const fs = require("fs");
 const { promisify } = require("util");
 const readFile = promisify(fs.readFile);
+const im = require("gm").subClass({ imageMagick: true });
 const app = Express();
 
 app.get("/", async (req, res) => {
@@ -9,8 +10,13 @@ app.get("/", async (req, res) => {
   if (filepath.endsWith("/")) filepath += "index.html";
   const buffer = await readFile("./static/" + filepath);
   const content = buffer.toString();
-  const newContent = content.split(/(<sc-img[^>]+><\/sc-img>)/);
-  console.log(newContent);
+  const newContent = content.split(/(<sc-img[^>]+><\/sc-img>)/).map(item => {
+    if (!item.startsWith("<sc-img")) return item;
+    const src = /src="([^"]+)"/.exec(item)[1];
+    const img = im("./static/" + src);
+    console.log(img);
+    return item;
+  });
   res.send(newContent.join(""));
 });
 app.use(Express.static("static"));
