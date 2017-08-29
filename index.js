b5d6132f2694bd22aa62a9d0298da0fc9930b4b9
Zain Fathoni <zain.fathoni@gmail.com>
2017-08-30 01:35:26 +0700

    intercept file reading

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const Express = require("express");
+const fs = require("fs");
+const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
 const app = Express();
 
+app.get("/", async (req, res) => {
+  let filepath = req.url;
+  if (filepath.endsWith("/")) filepath += "index.html";
+  const buffer = await readFile("./static/" + filepath);
+  const content = buffer.toString();
+  res.send(content);
+});
 app.use(Express.static("static"));
 app.listen(8080);
