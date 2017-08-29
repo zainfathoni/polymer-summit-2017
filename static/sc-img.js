67d10854ac60907cc572a0371af9945d67cffbb8
Zain Fathoni <zain.fathoni@gmail.com>
2017-08-30 00:48:46 +0700

    attach Shadow Root

--- static/sc-img.js
+++ static/sc-img.js
@@ -1,6 +1,17 @@
+const template = document.createElement("template");
+template.innerHTML = `
+    <style>
+        :host {
+            display: block;
+        }
+    </style>
+`;
+
 class SCImg extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
 
