ee267059518e01dc0c8f5257f725c792ed8fdae5
Zain Fathoni <zain.fathoni@gmail.com>
2017-08-30 01:05:03 +0700

    add Intersection Observer

--- static/sc-img.js
+++ static/sc-img.js
@@ -1,17 +1,55 @@
 const template = document.createElement("template");
 template.innerHTML = `
-    <style>
-        :host {
-            display: block;
-        }
-    </style>
+  <style>
+    :host {
+      display: block;
+    }
+  </style>
 `;
 
+const io = new IntersectionObserver(entries => {
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      entry.target.setAttribute("full", "");
+    }
+  }
+});
+
 class SCImg extends HTMLElement {
+  static get observedAttributes() {
+    return ["full"];
+  }
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    io.observe(this);
+  }
+
+  disconnectedCallback() {
+    io.unobserve(this);
+  }
+
+  get full() {
+    return this.hasAttribute("full");
+  }
+
+  get src() {
+    return this.getAttribute("src");
+  }
+
+  attributeChangedCallback() {
+    if (this.loaded) return;
+    const img = document.createElement("img");
+    img.src = this.src;
+    img.onload = _ => {
+      this.loaded = true;
+      this.shadowRoot.appendChild(img);
+    };
   }
 }
 
